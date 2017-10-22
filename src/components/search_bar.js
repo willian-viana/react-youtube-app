384d61e6e70f81d3b928dacce3119ac3033fb140
Willian Viana <willian.batista.viana@gmail.com>
2017-10-22 16:31:35 -0200

    changed SearchBarComponent to class based and iniciated state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 
 
 class SearchBar extends Component { 
+    constructor (props) {
+        super(props);
+
+        this.state = {term : ''};
+    }
+
     render() {
-        return <input onChange={this.onInputChange} />;
+        return (
+            <div>
+                <input onChange={ event => this.setState({ term: event.target.value }) } />
+                <p>Valor do input: { this.state.term } </p>
+            </div>
+        )
     }
 
-    onInputChange(event) {
-        console.log(event.target.value);
-
-    }
 };
 
 export default SearchBar;
